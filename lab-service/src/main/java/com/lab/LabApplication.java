b372694055e11747b12e40e761fce91bd46023a9
lyj <1083137397@qq.com>
2024-10-06 12:20:33 +0800

    引入openfeign

--- lab-service/src/main/java/com/lab/LabApplication.java
+++ lab-service/src/main/java/com/lab/LabApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @MapperScan(basePackages = {"com.lab.mapper"})
 @EnableAsync
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.lab.clients"})
 public class LabApplication {
 
     public static void main (String[] args) {
