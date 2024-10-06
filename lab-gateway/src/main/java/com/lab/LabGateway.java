a73d7a896d80d1284fa1b638774458cdec9939cd
lyj <1083137397@qq.com>
2024-10-06 12:59:01 +0800

    引入gateway

---
+++ lab-gateway/src/main/java/com/lab/LabGateway.java
@@ -0,0 +1,13 @@
+package com.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LabGateway {
+    public static void main (String[] args) {
+        SpringApplication.run(LabGateway.class, args);
+    }
+}
